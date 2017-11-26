083d60c0f980bf7dc24f05a3e55ebe387ebd780f
Jack Lightbody <jack.lightbody@gmail.com>
2017-11-26 13:12:21 -0500

    Splitting out into learn and use classes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, render_template,jsonify
 from goose import Goose
-from gensim.models.keyedvectors import KeyedVectors
-from gensim.models.lsimodel import LsiModel
+import nltk
 import unidecode
-import spotlight
 import operator
+import gensim
 from textblob import TextBlob
 
 app = Flask(__name__)
@@ -14,12 +13,33 @@ sim_threshold = top_features/3
 # and where the v1 is the top features
 # and v2 is the overall sentiment of the article
 articles = {}
+token_dict = {}
+f = open("stopwords.txt")
+stopwords = [w for w in f]
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello!"
- 
+
+
+
+def tokenize(tokens):
+    stemmer = nltk.stem.porter.PorterStemmer()
+    tokens = nltk.word_tokenize(text)
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+def clean_text(text):
+    filtered = [w for w in text if not w in stopwords]
+    lowers = filtered.lower()
+    no_punctuation = lowers.translate(None, string.punctuation)
+    token_dict[file] = no_punctuation
+
+
+tfidf_model = gensim.models.TfidfModel.load('tfidf.model')
+
 @app.route('/extract')
 def extract():
     url = request.args.get('url')
@@ -34,15 +54,11 @@ def extract():
         topic_score = blob.sentiment.polarity
         print("POLARITY")
         print(topic_score)
-        annotations = spotlight.annotate('http://model.dbpedia-spotlight.org/en/annotate', article.cleaned_text, confidence=0.4)
         ids = {}
-        for annotation in annotations:
-            annotation_id = annotation['URI']
-            if annotation_id in ids:
-                ids[annotation_id] += 1
-            else:
-                ids[annotation_id] = 1
-        print(ids)
+        doc_words = clean_text(text)
+        query_doc_bow = dictionary.doc2bow(doc_words)
+        query_doc_tf_idf = tf_idf[query_doc_bow]
+        sims = sims[query_doc_tf_idf]
         sorted_annotations = sorted(ids.items(), key=operator.itemgetter(1), reverse=True)[:top_features]
         annotations_set = set([a[0] for a in sorted_annotations])
         for article in articles:
