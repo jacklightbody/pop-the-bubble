761f0f5429d43eebc1ac8492d44f4cf2d8361cbf
Jack Lightbody <jack.lightbody@gmail.com>
2017-11-30 20:14:46 -0500

    adding sentiment analysis and topic modelling v1

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,3 +1,4 @@
+var wordThreshold = 100
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.pageLoaded) {
@@ -12,11 +13,46 @@ chrome.tabs.onActivated.addListener(
     }
 )
 function processPage(doc){
-    page = document.createElement('div');
+    var page = document.createElement('div');
     page.innerHTML = doc;
     readability.cleanStyles(page);
     readability.removeScripts(page);
+    removeImages(page)
     var article = readability.grabArticle(page);
     article = article.textContent || article.innerText;
-    console.log(article);
+    var wordCount =  article.split(" ").length;
+    if(wordCount > wordThreshold){
+        console.log(article);
+        console.log(getArticleTopicsAndSentiments(article));
+    }
+}
+function getArticleTopicsAndSentiments(text){
+    var sentiment = new Sentimood();
+    var articleSentiments = {};
+    var sentences = nlp(text).sentences().out('array');
+    var add, topic, sentenceSentiment, topics;
+    console.log(sentences)
+    for(var i = 0; i < sentences.length; i++){
+        topics = nlp(sentences[i]).topics().data();
+        sentenceSentiment = sentiment.analyze(sentences[i]);
+        for(var j = 0; j < topics.length; j++){
+            topic = topics[j].text
+            add = sentenceSentiment.score;
+            if(add == 0){
+                continue;
+            }
+            if(topic in articleSentiments){
+                articleSentiments[topic] = articleSentiments[topic] + add;
+            }else{
+                articleSentiments[topic] = add;
+            }
+        }
+    }
+    return articleSentiments;
+}
+function removeImages(doc){
+    var images = doc.getElementsByTagName('img');
+    for (var i = 0; i < images.length; i++) {
+        images[i].removeAttribute('src');
+    }
 }
